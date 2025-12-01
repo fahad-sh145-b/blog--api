999c9990a98994b174ccf4f03acbdacec9c2874f
fahad-sh145 <bs6816828@gmail.com>
2025-12-02 04:08:39 +0530

    Fix: added lodash dependency

--- db.js
+++ db.js
@@ -1,31 +1,17 @@
-
-
-
 const mongoose = require('mongoose');
+require('dotenv').config();
 
-// ✅ include database name at the end (important!)
-const mongoURL ='mongodb://localhost:27017/blogapi'
+const mongoURL = process.env.MONGODB_URL;
 
-// connect to MongoDB
 mongoose.connect(mongoURL, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
-// get default connection
 const db = mongoose.connection;
 
-// event listeners
-db.on('connected', () => {
-  console.log('✅ Connected to MongoDB server');
-});
-
-db.on('error', (err) => {
-  console.error('❌ MongoDB connection error:', err);
-});
-
-db.on('disconnected', () => {
-  console.log('⚠️ MongoDB disconnected');
-});
+db.on('connected', () => console.log('Connected to MongoDB Atlas'));
+db.on('error', (err) => console.error('MongoDB error:', err));
+db.on('disconnected', () => console.log('MongoDB disconnected'));
 
 module.exports = db;
